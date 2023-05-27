b51d5172096f83c520cde849cbac51b745225ce3
jamesbattam <134773204+jamesbattam@users.noreply.github.com>
2023-05-27 19:00:38 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,78 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <App />import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <App />
+  </StrictMode>import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
   </StrictMode>
 );
